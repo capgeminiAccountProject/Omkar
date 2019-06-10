b0a4efa5ffb4c87e4457c287a28451389cdb8362
ovedpath <ovedpath@LNAR-PC06M31S.corp.capgemini.com>
2019-06-10 11:00:17 -0500

    New Changes for logging

--- AccountProject/src/main/java/com/account/App.java
+++ AccountProject/src/main/java/com/account/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan(basePackages = "com.account")
--- AccountProject/src/main/java/com/account/entity/Account.java
+++ AccountProject/src/main/java/com/account/entity/Account.java
@@ -10,19 +10,22 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "ACCOUNT")
-
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
 public class Account {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO) 
 	public Long account_number;
@@ -33,17 +36,5 @@ public class Account {
 	public Date birth_date;
 	public String account_type;
 	public BigDecimal ammount;
-	protected Account() {}
-	public Account(Long account_number, String first_name, String last_name, Long ssn, Date birth_date,
-			String account_type, BigDecimal ammount) {
-		super();
-		this.account_number = account_number;
-		this.first_name = first_name;
-		this.last_name = last_name;
-		this.ssn = ssn;
-		this.birth_date = birth_date;
-		this.account_type = account_type;
-		this.ammount = ammount;
-	}
-	//: first name, last name, ssn, birthdate, account type, amount.
+	
 }
